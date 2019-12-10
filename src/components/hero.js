30c7129a717c1413884c6d90aa8e9275945c94f2
kevin <kevinahmad.f@gmail.com>
2019-12-10 13:35:07 +0700

    change to cuanin

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,7 +2,7 @@ import { Flex, Heading } from "@chakra-ui/core";
 
 const Hero = () => (
   <Flex justifyContent="center" alignItems="center" height="100vh">
-    <Heading fontSize="10vw">mathdroid</Heading>
+    <Heading fontSize="10vw">CuanIn</Heading>
   </Flex>
 );
 export default Hero;
